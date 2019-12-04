82a1bd1e69be42bb153f22bc9e971ad0c9317924
Fei <fei@reseller.visualhawaii.com>
2019-12-04 10:39:00 -1000

    Form enable function done

--- public/js/events.js
+++ public/js/events.js
@@ -2,41 +2,70 @@
 // 首先所有的名字要一致才能运作
 // 第二，要是加了什么event，要及时地把event的function加上，要不然也会无法继续实行
 // 三个字，好坑爹
-
 function init() {
     let _event = {};
 
-    // Get all items from the id
+    // Get all items from the form by their id
     const divForm = document.getElementById('divForm');
     const selStartTime = document.getElementById('startTime');
     const selEndTime = document.getElementById('endTime');
     const txtActivity = document.getElementById('activity');
     const txtLocation = document.getElementById('location');
     const txtNote = document.getElementById('note');
-    // const hdnId = document.getElementById('id');
-    // const hdnDate = document.getElementById('date');
     const loading = document.getElementById('loading');
     const btnDelete = document.getElementById('deleteButton');
     const btnClose = document.getElementById('closeButton');
     const btnSave = document.getElementById('saveButton');
+    const hiddenDate = document.getElementById('hiddenDate');
+
+    // Buuton Events
     divForm.addEventListener('submit', submitForm);
     // saveButton.addEventListener('click', saveEvent);
     deleteButton.addEventListener('click', deleteEvent);
     closeButton.addEventListener('click', closeForm);
+
     // 時間グリッドを押下時発生するイベントの設定 (要確認)
     const eventItem = document.querySelectorAll(
         '.detail-container .detail-item.available, .detail-container .detail-item.booked'
     );
-    alert('Hey000');
     for (let event of eventItem) {
         event.addEventListener('click', openForm);
     }
 
     // 時間グリッドを押下時form開くイベント
     function openForm(e) {
-        alert('Hey444');
+        // Get infos from the grid before open the form
+        const eventId = e.target.getAttribute('data-id') | 0;
+        const startTime = hiddenDate.value + ' ' + e.target.getAttribute('data-start') + ':00';
+        const endTime = hiddenDate.value + ' ' + e.target.getAttribute('data-end') + ':00';
+        const booked = e.target.getAttribute('data-booked') === 'true';
+        // Display form
         divForm.style.display = 'block';
-        console.log('how about hereeeeeee');
+        _event = {};
+
+        showLoading();
+        // Update
+        if (booked) {
+            // Show loading mark while getting the data
+            // Get the detail by eventId, set 1 second sleep time
+            setTimeout(() => {
+                fetchEvent(eventId)
+            }, 500);
+        }
+        // New Event
+        else {
+            _event = {
+                id: 0,
+                startTime,
+                endTime,
+                activity: '',
+                location: '',
+                note: '',
+            };
+            hideLoading();
+            showEvent();
+        };
+
     }
 
     function submitForm(e) {
@@ -47,8 +76,58 @@ function init() {
 
     }
 
+    // Hide the Form
     function closeForm(e) {
+        divForm.style.display = 'none';
+    }
 
+    // Show Loading mark 
+    function showLoading() {
+        loading.style.display = 'grid';
+    }
+
+    // Hide the Loading mark
+    function hideLoading() {
+
+        loading.style.display = 'none';
+    }
+
+    // Get event info by eventId
+    function fetchEvent(id) {
+        // *This is GET method
+        const url = `/events?id=${id}`;
+
+        return fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                _event = data.length ? data[0] : null;
+                hideLoading();
+                showEvent();
+            })
+            .catch(err => {
+                console.error(err);
+                hideLoading();
+                // eslint-disable-next-line no-undef
+                Notiflix.Report.Failure('Error', err.message);
+            });
+    }
+
+    // 取得したイベント情報を再表示する
+    function showEvent() {
+        selStartTime.value = getTimeStr(_event.startTime);
+        selEndTime.value = getTimeStr(_event.endTime);
+        txtActivity.value = _event.activity;
+        txtLocation.value = _event.location;
+        txtNote.value = _event.note;
+
+        // Enable Delete button
+        btnDelete.style.display = _event.id ? 'inline' : 'none';
+    }
+
+    function getTimeStr(d) {
+        return d.split(' ')[1].substr(0, 5);
     }
 }
 
--- routes/events.js
+++ routes/events.js
@@ -6,131 +6,112 @@ const db = require('../db/models');
 const { DateTime } = require('luxon');
 const auth = require('./auth-helper');
 
-// // Get: From Home Page with the Date(?)
-// router.get('/', auth, async function (req, res, next) {
-//     try {
-//         // Get the data of that day
-//         const date = DateTime.fromISO(req.query['d']);
-//         if (!date.isValid) {
-//             return;
-//         }
-
-//         const eventList = await db.Event.findAll({
-//             where: {
-//                 startTime: { [Op.gte]: date.toJSDate() },
-//                 endTime: { [Op.lt]: date.plus({ days: 1 }).toJSDate() },
-//             },
-//             order: [
-//                 ['available', 'DESC'],
-//                 ['startTime', 'ASC'],
-//             ]
-//         });
-
-//         res.json(eventList);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send(`Error ${error.message}`);
-//         return;
-//     }
-// });
-
-// // Pass from the Home Page
-// router.get('/:year/:month/:day', auth, async (req, res) => {
-
-// });
-
+//#region --- Get event data by date ---
 // Get: From Home Page with the Date
 router.get('/', auth, async function (req, res, next) {
-    const date = DateTime.fromISO(req.query['d']);
-    if (!date.isValid) {
-        return;
-    }
-    const nextDay = date.plus({ days: 1 });
-
-    const times = [];
-    // Time label is every 30 mins, from 8am to 23pm
-    for (let i = 8; i < 23; i++) {
-        // 整点
-        const clock = {
-            // Display label
-            label: '' + i,
-            startTime: date.plus({ hours: i }).toFormat('HH:mm'),
-            endTime: date.plus({ hours: i, minutes: 30 }).toFormat('HH:mm'),
-            available: false,
-            eventId: 0,
-        };
-        times.push(clock);
-
-        // 半点
-        const half = {
-            label: '',
-            startTime: date.plus({ hours: i, minutes: 30 }).toFormat('HH:mm'),
-            endTime: date.plus({ hours: i + 1 }).toFormat('HH:mm'),
-            available: false,
-            eventId: 0,
-        };
-        times.push(half);
-    };
-
     try {
-        // All events of that day
-        var dayEvents = await db.Event.findAll({
-            where: {
-                startTime: {
-                    [Op.gte]: date.toJSDate()
+        let where = {};
+
+        // Get the date
+        const date = DateTime.fromISO(req.query['d']);
+        // If it's the GET method by date
+        if (date.isValid) {
+            const nextDay = date.plus({ days: 1 });
+
+            const times = [];
+            // Time label is every 30 mins, from 8am to 23pm
+            for (let i = 8; i < 23; i++) {
+                // 整点
+                const clock = {
+                    // only for display on the header
+                    label: '' + i,
+                    // Event selectable time, for the pulldown display
+                    startTime: date.plus({ hours: i }).toFormat('HH:mm'),           // 08:00
+                    endTime: date.plus({ hours: i, minutes: 30 }).toFormat('HH:mm'),    // 08:30
+                    // Available time or not
+                    available: false,
+                    // Related eventId
+                    eventId: 0,
+                };
+                times.push(clock);
+
+                // 半点
+                const half = {
+                    label: '',
+                    startTime: date.plus({ hours: i, minutes: 30 }).toFormat('HH:mm'),  // 08:30
+                    endTime: date.plus({ hours: i + 1 }).toFormat('HH:mm'), // 09:00
+                    available: false,
+                    eventId: 0,
+                };
+                times.push(half);
+            };
+
+            // All events of that day
+            var dayEvents = await db.Event.findAll({
+                where: {
+                    startTime: {
+                        [Op.gte]: date.toJSDate()
+                    },
+                    endTime: {
+                        [Op.lt]: nextDay.toJSDate()
+                    }
                 },
-                endTime: {
-                    [Op.lt]: nextDay.toJSDate()
-                }
-            },
-            order: [
-                ['available', 'DESC'],
-            ]
-        });
+                order: [
+                    ['available', 'DESC'],
+                ]
+            });
 
-        // Separate every event of that day
-        for (const i of dayEvents) {
-            // console.log(JSON.stringify(i));
-            const timeFrom = DateTime.fromSQL(i.startTime).toFormat('HH:mm');
-            const timeTo = DateTime.fromSQL(i.endTime).toFormat('HH:mm');
+            // Separate every event of that day
+            for (const i of dayEvents) {
+                // console.log(JSON.stringify(i));
+                const timeFrom = DateTime.fromSQL(i.startTime).toFormat('HH:mm');
+                const timeTo = DateTime.fromSQL(i.endTime).toFormat('HH:mm');
 
-            // const filteredTimes = times.filter(
-            //     t => t.startTime >= timeFrom && t.endTime <= timeTo
-            // );
-            // for (const t of filteredTimes) {
-            //     t.available = i.available;
-            //     t.eventId = i.id;
-            //     t.booked = !i.available;
-            // }
+                // Update info for every event
+                times.filter(
+                    t => t.startTime >= timeFrom && t.endTime <= timeTo
+                ).forEach(t => {
+                    t.available = i.available;
+                    t.booked = !i.available;
+                    t.eventId = i.id;
+                });
+            }
 
-            // Update info for every event
-            times.filter(
-                t => t.startTime >= timeFrom && t.endTime <= timeTo
-            ).forEach(t => {
-                t.available = i.available;
-                t.booked = !i.available;
-                t.eventId = i.id;
-                t.from = timeFrom;
-                t.to = timeTo;
+            // Only avail select time will be showed in pulldown list
+            const timesForSelect = times.filter(t => t.eventId !== 0);
+            // Which day has been selected
+            const fullDate = date.toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });
+            const hiddenDate = date.toFormat('yyyy-MM-dd');
+            res.render('events', {
+                fullDate,
+                times,
+                timesForSelect,
+                dayEvents,
+                hiddenDate
             });
         }
+        // GET method by eventId
+        else {
+            const id = req.query['id'] | 0;
+            if (id) {
+                where = { ...where, id };
+            }
 
-        // Only avail select time will be showed in pulldown list
-        const timesForSelect = times.filter(t => t.eventId !== 0);
-        // Which day has been selected
-        const fullDate = date.toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });
-
-        res.render('events', {
-            fullDate,
-            times,
-            timesForSelect,
-            dayEvents
-        });
+            const list = await db.Event.findAll({
+                where,
+                order: [
+                    ['available', 'DESC'],
+                    ['startTime', 'ASC'],
+                ],
+            });
+            res.json(list);
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send(`Error ${error.message}`);
         return;
     }
 });
+//#endregion
 
 module.exports = router;
